9b5c4652f78e78e0b902d104a4243ae0f9a31ae0
Vivek Booshan <vivek.booshan@gmail.com>
2026-02-23 18:21:03 -0500

    expose lattice_soa as lattice

--- src/NuLattice/__init__.py
+++ src/NuLattice/__init__.py
@@ -3,9 +3,12 @@
 #     MASS,
 # )
 
+from . import lattice_soa as lattice
 from ._types import (
     OneBodyOperator,
     TwoBodyOperator,
     ThreeBodyOperator,
 )
-__all__ = ["OneBodyOperator", "TwoBodyOperator", "ThreeBodyOperator"]
+__all__ = ["OneBodyOperator", "TwoBodyOperator", "ThreeBodyOperator", "lattice"]
+
+
